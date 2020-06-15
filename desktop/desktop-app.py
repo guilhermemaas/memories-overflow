49de72cd77b449de68afd47714227cfa6c3a6b8a
guilhermemaas <guilherme.maas@gmail.com>
2020-06-15 01:00:02 -0300

    ajuste interface app desktop

--- desktop/desktop-app.py
+++ desktop/desktop-app.py
@@ -1,29 +1,38 @@
 import PySimpleGUI as sg
+import subprocess
+import os
+import time
+import sys
+import threading
 import queue
+from enum import Enum
+import requests
 
-sg.theme('DarkAmber')
+sg.theme('DarkBrown4')
 
 def main():
 
     gui_queue = queue.Queue()
 
     layout = [
-        [sg.Image(r'C:\Users\guilh\Documents\dev\net-test\images\nettest.png')],
+        [sg.Image(r'C:\Users\guilh\Documents\dev\memories-overflow\desktop\images\topo_logo2.png')],
         #[sg.Text('Diretório de saída:', size=(15, 1)), sg.InputText(), sg.FolderBrowse()],
         #[sg.Text('Endereço alternativo:', size=(15, 1)) ,sg.InputText()],
         [sg.Output(size=(110,30), background_color='black', text_color='white')],
-        [sg.InputText(size=110), sg.Button('Enviar'), sg.Button('Sair')],
-        [sg.Text('github.com/guilhermemaas/memories-overflow')]
+        [sg.Button('Enviar'), sg.Button('Sair')],
+        [sg.Text('github.com/guilhermemaas/memories-overflow')],
         #[sg.Text('unifique.com.br')]
     ]
 
-    window = sg.Window('Memories: Overflow', layout, no_titlebar=False, grab_anywhere=False)
+    window = sg.Window('Memories: Overflow', layout)
 
     while True:        
         event, values = window.read(timeout=15)
 
+        #out_dir = values[1].replace('/', '\\')
+
         if event == 'Enviar':
-            pass
+            print('Hello Fantasy World!')
             
         try:
             message = gui_queue.get_nowait()    # see if something has been posted to Queue
